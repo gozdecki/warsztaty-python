bf4956411a4515c9e78319b1a2cf8f6f7cdfcc60
Janusz Gozdecki <gozdecki@agh.edu.pl>
2016-11-15 15:42:46 +0100

    tryg #3

--- trygonometria.py
+++ trygonometria.py
@@ -15,19 +15,23 @@ def sin(x):
 
 
 def cos(x):
-   if x == 90:
-      res = 0
+   if x == 0:
+      res = 1
    elif x == 30:
       res = (3**(0.5))/2
    elif x == 45:
       res = (2**(0.5))/2
    elif x == 60:
       res = 0.5
-   elif x == 0:
-      res = 1
+   elif x == 90:
+      res = 0
    else:
       res = False
    return res
 
+def cos2(x):
+   return sin(90-x)
+
 print(sin(90), sin(45), sin(20))
 print(cos(90), cos(45), cos(20))
+print(cos2(90), cos2(45), cos2(20))
